3034d76b99e3edef9e8719a38523c8c6ce80cf47
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-09-09 21:32:01 +0300

    react

---
+++ react/test-2 template/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="main-footer">&copy; Vako - 2018</footer>
+    );
+  }
+}
+
+export default Footer;
